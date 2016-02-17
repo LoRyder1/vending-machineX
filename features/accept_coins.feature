fc6e854662a5489cd031ed652cdbde02bed76bac
Lovinder Pnag <lspnag@gmail.com>
2016-02-17 17:25:47 -0500

    step definitions for cucumber tests

--- features/accept_coins.feature
+++ features/accept_coins.feature
@@ -4,7 +4,7 @@ I want a vending machine that accepts coins
 So that I can collect money from the customer
 
   Scenario: Accept valid coins (nickels, dimes, and quarters)
-  Given I insert a nickel into vending machine
-  Then the value of the coin will be added to current amount
+  Given I vend and insert a nickel into vending machine
+  Then the value of the nickel will be added to current amount
   When I insert a penny into vending machine
   Then the coin will be rejected
---
+++ features/step_definitions/vendmachine_steps.rb
@@ -0,0 +1,19 @@
+NICKEL = Coin.new(3,3)
+PENNY = Coin.new(1,1)
+  
+Given(/^I vend and insert a (\S[a-z]*) into vending machine$/) do |coin|
+  @vend = VendingMachine.new
+  @vend.insert_coin(coin.upcase)
+end
+
+Then(/^the value of the (\S[a-z]*) will be added to current amount$/) do |coin|
+  expect(@vend.current_amount).to eq coin.upcase.value
+end
+
+When(/^I insert a (\S[a-z]*) into vending machine$/) do |coin|
+  @vend.insert_coin(coin.upcase)
+end
+
+Then(/^the coin will be rejected/) do
+  expect(@vend.display).to eq 'REJECTED COIN'
+end
