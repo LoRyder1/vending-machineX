f67180181694b340f6b8e04e8e3be49f18fb0277
Lovinder Pnag <lspnag@gmail.com>
2016-02-17 17:31:57 -0500

    Coin class takes in two arguments

--- spec/vendmachine_spec.rb
+++ spec/vendmachine_spec.rb
@@ -3,11 +3,17 @@ describe 'VendingMachine' do
   subject {VendingMachine.new}
 
   describe '#insert_coin' do
-    it 'is defined as a method' do
+    xit 'is defined as a method' do
       expect(VendingMachine.method_defined?(:insert_coin)).to eq true
     end
   end
 end
 
 describe 'Coin' do
+  let(:num) {double('num')}
+  subject {Coin.new(num, num)}
+
+  it 'expects to take in two arguments' do
+    expect(subject.method(:initalize).arity).to eq 2
+  end
 end
